24e5e16cdf1c94a891690528cce684f1931a7d9b
robhogfeldt-fron15 <robhogfeldt@hotmail.com>
2015-11-02 12:25:23 +0100

    bootstrap base

--- app/components/common/header.js
+++ app/components/common/header.js
@@ -7,18 +7,73 @@ class Header extends React.Component {
   render(){
 
     return (
-      <div className="container">
-          <nav className='navbar navbar-default'>
+      <div>
+      <div className="col-md-12">
+                        <a href="#">
+                        <div className="logo text-center">
+                            <h1>reactive dogs</h1>
+                            <span>Handle your dogs with ReactJs</span>
+                        </div>
+                        </a>
+                    </div>
 
-              <ul className='nav navbar-nav'>
-                <li><a href='/#'>Home</a></li>
-                <li><a href='/#/mydogs'>MyDogs</a></li>
-                <li><a href='/#/myevents'>MyEvents</a></li>
-                <li><a href='/#/setup'>Prepare Event</a></li>
-              </ul>
+      <div className="mainbody-section text-center col-md-12">
+                <div className="container">
+                    <div className="row">
 
-          </nav>
-      </div>
+                        <div className="col-md-3">
+
+                            <div className="menu-item blue">
+                                <a href="/#" data-toggle="modal">
+                                    <i className="fa fa-magic"></i>
+                                    <p>Home</p>
+                                </a>
+                            </div>
+
+
+
+                        </div>
+
+                        <div className="col-md-3">
+                                    <div className="menu-item color">
+                                        <a href="/#/myevents" data-toggle="modal">
+                                            <i className="fa fa-area-chart"></i>
+                                            <p>Events</p>
+                                        </a>
+                                    </div>
+
+
+                        </div>
+
+                        <div className="col-md-3">
+
+                            <div className="menu-item light-orange">
+                                <a href="/#/mydogs" data-toggle="modal">
+                                    <i className="fa fa-users"></i>
+                                    <p>Team</p>
+                                </a>
+                            </div>
+
+
+
+                        </div>
+
+                        <div className="col-md-3">
+
+                            <div className="menu-item light-red">
+                                <a href="/#/setup" data-toggle="modal">
+                                    <i className="fa fa-envelope-o"></i>
+                                    <p>Set Up</p>
+                                </a>
+                            </div>
+
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+          </div>
     );
 
   }
